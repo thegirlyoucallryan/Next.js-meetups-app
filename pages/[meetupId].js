da9231a89690d0075da0ca229f658024df8dc568
Ryan Hartley <ryan@elleestphotography.com>
2021-09-24 14:28:29 -0400

    change fallback

--- pages/[meetupId].js
+++ pages/[meetupId].js
@@ -39,7 +39,7 @@ export async function getStaticPaths() {
 
 
   return{
-    fallback: false,
+    fallback: 'blocking',
     paths: meetups.map((meetup) => ({
       params: {meetupId: meetup._id.toString() },
     })),
